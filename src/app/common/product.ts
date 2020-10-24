02eb7f525f012e079b90a3f665db500301ff8d58
Vinayak <vinayakchaurasia99@gmail.com>
2020-10-24 22:12:01 +0530

    Added Product class

---
+++ src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+    sku:String;
+    name:String;
+    description:String;
+    unitPrice:number;
+    imageUrl:String;
+    active:boolean;
+    unitsInStock:number;
+    dateCreated:Date;
+    lastUpdated:Date;
+
+}
