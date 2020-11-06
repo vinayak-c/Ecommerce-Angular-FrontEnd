5e8b29924dde5c11e495a1cddae2dcca789c2a96
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-06 15:15:46 +0530

    Added method to handle Searched Products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,11 +12,12 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
@@ -24,14 +25,38 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    //search for the products using keywords
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProducts() {
+
     //check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     }
-    else{
-      this.currentCategoryId=1;
+    else {
+      this.currentCategoryId = 1;
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
@@ -40,5 +65,4 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
-
 }
