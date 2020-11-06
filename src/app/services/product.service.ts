61390a768e72760f74cccc890e6c81ed4d94cbc9
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-06 21:22:56 +0530

    Added Method For Single Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,6 +16,15 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getProduct(theProductId: number): Observable<Product> {
+
+    //need to build product url
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
