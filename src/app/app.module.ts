0b632450f87ffd36f72592cada53d7dfe5477ea5
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-06 15:09:43 +0530

    Added route for Search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
-
 import {Routes,RouterModule} from '@angular/router';
 
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+
 const routes: Routes=[
+  {path:'search/:keyword',component:ProductListComponent},
   {path:'category/:id',component:ProductListComponent},
   {path:'category',component:ProductListComponent},
   {path:'products',component:ProductListComponent},
   {path:'',redirectTo:'/products',pathMatch:'full'},
-  {path:'**',redirectTo:'/products',pathMatch:'full'},
+  {path:'**',redirectTo:'/products',pathMatch:'full'}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
