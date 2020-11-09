47edcb4cbb76c2be78caaa0a01d5e14a679c9289
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-09 14:39:59 +0530

    Added methods for Pagination Support

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,8 +22,20 @@ export class ProductService {
     const productUrl = `${this.baseUrl}/${theProductId}`;
 
     return this.httpClient.get<Product>(productUrl);
-  
+
   }
+
+  //URL based on categoryId,Page and Size
+  getProductListPaginate(thePage: number,
+    thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
@@ -35,6 +47,17 @@ export class ProductService {
   searchProducts(theKeyword: string): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
     return this.getProducts(searchUrl);
+  }
+
+  searchProductsPaginate(thePage: number,
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts> {
+
+    //need to built URL based on keyword,page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
@@ -51,9 +74,17 @@ export class ProductService {
 }
 
 interface GetResponseProducts {
+
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
+
 }
 
 interface GetResponseProductCategory {
