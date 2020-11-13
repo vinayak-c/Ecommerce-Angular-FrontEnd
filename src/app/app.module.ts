edb7d28f1e85a3bb64a27735439689fe9f06b27d
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-13 19:39:32 +0530

    Included checkoutcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes=[
   {path:'checkout',component:CheckoutComponent},
@@ -37,7 +37,8 @@ const routes: Routes=[
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
