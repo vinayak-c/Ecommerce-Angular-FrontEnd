62c883d793c099192e6ceac16f18302006bb9dc1
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-13 19:45:56 +0530

    Added multiple form groups

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  checkoutFormGroup:FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.checkoutFormGroup=this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName:[''],
+        lastName:[''],
+        email:['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street:[''],
+        city:[''],
+        state:[''],
+        country:[''],
+        zipCode:['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street:[''],
+        city:[''],
+        state:[''],
+        country:[''],
+        zipCode:['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType:[''],
+        nameOnCard:[''],
+        cardNumber:[''],
+        securityCode:[''],
+        expirationMonth:[''],
+        expirationYear:['']
+      })
+    });
+
+  }
+
+  onSubmit(){
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer').value);
   }
 
 }
