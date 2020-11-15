c8472e4eb7722cdea572f1590aabf50594ce3343
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-15 21:04:34 +0530

    Added Year and Months methods

--- src/app/services/vinayak-shop-form.service.ts
+++ src/app/services/vinayak-shop-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,33 @@ import { Injectable } from '@angular/core';
 export class VinayakShopFormService {
 
   constructor() { }
+
+  getCreditCardMonths(startMonth:number):Observable<number[]>{ 
+
+    let data:number[]=[];
+
+    //build an array for "Month" dropdown list
+
+    for(let theMonth=startMonth;theMonth<=12;theMonth++){
+      data.push(theMonth);
+    }
+
+    return of(data); 
+  }
+
+  getcreditcardYears():Observable<number[]>{
+
+    let data:number[]=[];
+
+    //build an array for "Year" dropdown list for next 10 years
+
+    const startYear:number=new Date().getFullYear();
+    const endYear:number=startYear+10;
+
+    for(let theYear=startYear;theYear<=endYear;theYear++){
+       data.push(theYear);
+    }
+
+    return of(data);
+  }
 }
