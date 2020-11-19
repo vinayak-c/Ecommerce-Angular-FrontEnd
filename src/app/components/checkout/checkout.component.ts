3e33f66113786b747c88eaa63d13df517ac882a2
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-19 15:15:36 +0530

    Added validation for customer group

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { VinayakShopFormService } from 'src/app/services/vinayak-shop-form.service';
@@ -19,21 +19,21 @@ export class CheckoutComponent implements OnInit {
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
 
-  countries:Country[]=[]; 
+  countries: Country[] = [];
 
-  shippingAddressStates:State[]=[];
-  billingAddressStates:State[]=[];
-  
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder,
     private vinayakshopformservice: VinayakShopFormService) { }
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -59,7 +59,6 @@ export class CheckoutComponent implements OnInit {
       })
     });
 
-
     //populate credit card months
     const startMonth: number = new Date().getMonth() + 1;
     console.log("start month: " + startMonth);
@@ -81,9 +80,9 @@ export class CheckoutComponent implements OnInit {
 
     //populate countries
     this.vinayakshopformservice.getCountries().subscribe(
-      data=>{
-        console.log("Retrived countries: "+JSON.stringify(data));
-        this.countries=data; 
+      data => {
+        console.log("Retrived countries: " + JSON.stringify(data));
+        this.countries = data;
       }
     );
 
@@ -91,9 +90,14 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
     console.log("Handling the submit button");
+
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+    
     console.log(this.checkoutFormGroup.get('customer').value);
-    console.log("The shipping address country is: "+this.checkoutFormGroup.get('shippingAddress').value.country.name);
-    console.log("The shipping address state is: "+this.checkoutFormGroup.get('shippingAddress').value.state.name);
+    console.log("The shipping address country is: " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is: " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event) {
@@ -104,15 +108,19 @@ export class CheckoutComponent implements OnInit {
         setValue(this.checkoutFormGroup.controls.shippingAddress.value);
 
       //bug fix for states
-      this.billingAddressStates=this.shippingAddressStates;
+      this.billingAddressStates = this.shippingAddressStates;
 
     } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
 
       //bug fix for states
-      this.billingAddressStates=[];
+      this.billingAddressStates = [];
     }
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
 
   handleMonthsAndYears() {
 
@@ -133,31 +141,31 @@ export class CheckoutComponent implements OnInit {
     }
 
     this.vinayakshopformservice.getCreditCardMonths(startMonth).subscribe(
-      data=>{
-        console.log("Retrieved credit card months: "+JSON.stringify(data));
-        this.creditCardMonths=data;
-      }    
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
     );
   }
 
-  getStates(formGroupName:string){
+  getStates(formGroupName: string) {
 
-    const formGroup=this.checkoutFormGroup.get(formGroupName);
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
 
-    const countryCode=formGroup.value.country.code;
-    const countryName=formGroup.value.country.name;
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
 
     console.log(`${formGroupName} country code: ${countryCode}`);
     console.log(`${formGroupName} country name: ${countryName}`);
 
     this.vinayakshopformservice.getStates(countryCode).subscribe(
-      data=>{
-         
-        if(formGroupName==='shippingAddress'){
-          this.shippingAddressStates=data;
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
         }
-        else{
-          this.billingAddressStates=data;
+        else {
+          this.billingAddressStates = data;
         }
 
         //select first item by default
