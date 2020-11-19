8899cb47bd02ca040567e97332f3222f84f71166
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-19 16:53:36 +0530

    Added Space validator

---
+++ src/app/validators/vinayak-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { VinayakShopValidators } from './vinayak-shop-validators';
+
+describe('VinayakShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new VinayakShopValidators()).toBeTruthy();
+  });
+});
