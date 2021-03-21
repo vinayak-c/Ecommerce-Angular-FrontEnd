4b6ae3c2c25d3da3a4d539ed1b4f61161d0ff593
Vinayak <vinayakchaurasia99@gmail.com>
2021-03-22 02:53:26 +0530

    Added OKTA Config

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId:'0oabpm54gbEOwSTxp5d6',
+        issuer:'https://dev-27810128.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid', 'profile' ,'email']
+    }
+}
