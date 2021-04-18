d4b186a8e3c17b69415e0e7ca7d2a52a4b535384
Vinayak <vinayakchaurasia99@gmail.com>
2021-04-19 00:42:16 +0530

    Added Okta sign up feature and sign in feature

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { OktaAuthService } from '@okta/okta-angular';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 
+
+
 import myAppConfig from '../../config/my-app-config';
 
 @Component({
@@ -16,8 +18,11 @@ export class LoginComponent implements OnInit {
   constructor(private oktaAuthService: OktaAuthService) {
     this.oktaSignin = new OktaSignIn({
       logo: 'assets/images/logo.png',
-      baseUrl: myAppConfig.oidc.issuer.split('/aouth2')[0],
-      clienId: myAppConfig.oidc.clientId,
+      features: {
+        registration: true
+      },
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,
       authParams: {
         pkce: true,
@@ -31,8 +36,9 @@ export class LoginComponent implements OnInit {
 
     this.oktaSignin.remove();
 
-    this.oktaSignin.renderEL(
-      { el: '#okta-sign-in-widget' }, //this name should be same as div tag id in login.component.html
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'
+    }, // this name should be same as div tag id in login.component.html
       (response) => {
         if (response.status === 'SUCCESS') {
           this.oktaAuthService.signInWithRedirect();
