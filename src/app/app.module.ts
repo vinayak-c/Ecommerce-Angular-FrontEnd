42d75fe993167656627298c2f432a6f3b68a9651
Vinayak <vinayakchaurasia99@gmail.com>
2021-04-19 00:51:31 +0530

    Added Okta routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
-import {Routes,RouterModule} from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
 
-const routes: Routes=[
-  {path:'checkout',component:CheckoutComponent},
-  {path:'cart-details',component:CartDetailsComponent},
-  {path:'products/:id',component:ProductDetailsComponent},
-  {path:'search/:keyword',component:ProductListComponent},
-  {path:'category/:id',component:ProductListComponent},
-  {path:'category',component:ProductListComponent},
-  {path:'products',component:ProductListComponent},
-  {path:'',redirectTo:'/products',pathMatch:'full'},
-  {path:'**',redirectTo:'/products',pathMatch:'full'}
-]
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent
+} from '@okta/okta-angular';
+import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+
+const oktaConfig = Object.assign({
+
+  onAuthRequired: (injector) => {
+    const router = injector.get(Router);
+
+    //Redirect user to custom login page
+    router.navigate(['/login']);
+  }
+}, myAppConfig.oidc);
+
+const routes: Routes = [
+
+  { path: 'login/callback', component: OktaCallbackComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'cart-details', component: CartDetailsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -42,16 +64,18 @@ const routes: Routes=[
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: oktaConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
