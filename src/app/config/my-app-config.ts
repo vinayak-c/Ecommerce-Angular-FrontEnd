8430336f9dae3245717872c655eba42b1db79f4a
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-06 14:02:55 +0530

    Updated configs for HTTPS

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId:'0oabpm54gbEOwSTxp5d6',
         issuer:'https://dev-27810128.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes:['openid', 'profile' ,'email']
     }
 }
